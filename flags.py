74a827179e1d6431f1e5854efa463007fd19a1f5
atthematyo <johnlove1996@gmail.com>
2021-01-20 20:36:18 -0800

    Update flags.py

--- flags.py
+++ flags.py
@@ -1,3 +1,5 @@
+# Python 3.8.6
+
 # 8.0.1
 from PIL import Image
 
@@ -24,23 +26,31 @@ def save_to_tga(input_image: Image.Image, path: str) -> None:
     image.save(path)
 
 
-def make_flags(path_to_image: str, tag: str) -> None:
+def make_flags(path_to_image: str, tag: str, output_dir: str = 'output') -> None:
     img_base = Image.open(path_to_image).convert('RGBA')
 
     # Make 82 by 52 flag tga
-    make_folder('output')
+    make_folder(f'{output_dir}')
     img_large = img_base.resize((82, 52))
-    save_to_tga(img_large, f'output/{tag}')
+    save_to_tga(img_large, f'{output_dir}/{tag}')
 
     # Make 41 by 26 flag tga
-    make_folder('output/medium')
+    make_folder(f'{output_dir}/medium')
     img_med = img_base.resize((41, 26))
-    save_to_tga(img_med, f'output/medium/{tag}')
+    save_to_tga(img_med, f'{output_dir}/medium/{tag}')
 
     # Make 10 by 7 flag tga
-    make_folder('output/small')
+    make_folder(f'{output_dir}/small')
     img_small = img_base.resize((10, 7))
-    save_to_tga(img_small, f'output/small/{tag}')
+    save_to_tga(img_small, f'{output_dir}/small/{tag}')
+
+
+def make_many_flags(input_dir: str, output_dir: str) -> None:
+    files = os.listdir(input_dir)
+    for file in files:
+        if '.png' in file:
+            tag_ = file.split('.')[0]
+            make_flags(f'{input_dir}/{file}', tag_, output_dir)
 
 
 def main():
